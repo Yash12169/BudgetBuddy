7c00017f0414c3569b67b1f0e8eb4105e8f961c2
yash <jewalkaryash@gmail.com>
2025-03-02 20:04:14 +0530

    feat(UI): created signin and signup forms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,7 @@ import Faq from "@/components/FAQ/faq";
 import Features from "@/components/Features/features";
 import LandFooter from "@/components/Footer/LandFooter/landFooter";
 import Hero from "@/components/Hero/hero";
-import { LoginForm } from "@/components/SignUp/loginForm";
 import Navbar2 from "@/components/Navbar/navbar2";
-import ExampleWrapper from "@/components/ui/modal";
 import { TextParallax } from "@/components/ui/textParallax";
 import { ReactLenis } from "@studio-freight/react-lenis"; // Correct import
 
