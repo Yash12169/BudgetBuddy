69829a60fc1b14be8fe9fb37f1989e7238944685
yash-maurya23 <157914898+yash-maurya23@users.noreply.github.com>
2025-04-07 12:05:32 +0530

    Update database schema and API routes for emergency fund feature

---
+++ prisma/migrations/20250406182414_remove_amount_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `amount` on the `emergencyFund` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "emergencyFund" DROP COLUMN "amount";
