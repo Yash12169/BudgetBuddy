fdd08a0309a7013f6c66d7f44ae14c5fa981f718
yash-maurya23 <157914898+yash-maurya23@users.noreply.github.com>
2025-06-11 11:18:26 +0530

    restored

--- prisma/migrations/20250526093744_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId]` on the table `debt` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "debt_userId_key" ON "debt"("userId");
