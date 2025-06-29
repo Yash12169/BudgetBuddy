759b7cb7e36bfdf9eb543bcef707f6414df11773
Yash12169 <jewalkaryash@gmail.com>
2025-06-29 21:25:21 +0530

    fix(config): rolled back few changes

--- src/components/Cards/Debt.tsx
+++ src/components/Cards/Debt.tsx
@@ -40,7 +40,7 @@ export default function Debt() {
 
   const handleReCheck = () => {
     setIsNavigating(true);
-    router.push("/user/financial-checkup");
+    router.push("/user/financial-checkup/debt");
   };
 
   if (!debt) {
